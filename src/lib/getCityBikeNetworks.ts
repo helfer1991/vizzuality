9f5958fd65adcf1990c812bcc18020cbf4a44b87
helder-ferreira <1171482@isep.ipp.pt>
2024-12-03 20:51:58 +0000

    fix: fix linting issues

--- src/lib/getCityBikeNetworks.ts
+++ src/lib/getCityBikeNetworks.ts
@@ -1,14 +1,9 @@
 import type { CityBikeNetwork } from '@/types';
-import cityBikeNetworkMock from '@/__mocks__/networksMock.json';
 import { CITYBIKES_API_URL } from './constants';
 
 export const getCityBikeNetworks = async (): Promise<
 	Array<CityBikeNetwork>
 > => {
-	if (process.env.NEXT_PUBLIC_USE_MOCKS === 'true') {
-		return cityBikeNetworkMock.networks;
-	}
-
 	try {
 		const response = await fetch(CITYBIKES_API_URL);
 
